8b1791e0e720ad7e89a8d3f006712de1045098e9
YJ-Y <75804468+YJ-Y@users.noreply.github.com>
2025-07-28 19:17:52 +0900

    dev 브랜치 덮어씌우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import MyProfile from './pages/MyProfile/MyProfile';
+import SignUp from "./pages/SignUp/SignUp";
+import Main from "./pages/MainBoard/Main";
 
 
 function App() {
@@ -8,10 +10,10 @@ function App() {
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/myprofile" element={<MyProfile />} />
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/main" element={<Main />} />
     </Routes>
   )
 }
 
-
-
-export default App
+ export default App;
--- src/api/auth.js
+++ src/api/auth.js
@@ -1,15 +1,31 @@
-import axios from 'axios'
-
-const BASE_URL = 'https://your-backend-url.com/api'  // 백엔드 주소로 변경
+import axiosInstance from './axiosInstance';  // axiosInstance.js 파일에서 axios 인스턴스를 import
 
 export const login = async (id, password) => {
   try {
-    const response = await axios.post(`${BASE_URL}/login`, {
-      id,
+    const response = await axiosInstance.post('/auth/login', {
+      loginId,
       password
-    })
-    return response.data
+    });
+    return response.data;  // 서버에서 받은 데이터 반환
   } catch (error) {
-    throw error.response?.data || error
+    throw error.response?.data || error;  // 에러 처리
   }
 }
+
+export const checkNickname = (nickname) =>
+  axiosInstance.post('users/check-nickname', { nickname : '' });
+
+export const checkUsername = (loginId) =>
+  axiosInstance.post('users/check-loginId', { loginId : '' });
+
+export const sendVerificationEmail = (email) =>
+  axiosInstance.post('/email/send-code', { email });
+
+export const verifyCode = (email, code) =>
+  axiosInstance.post('/email/verify-code', { email, code });
+
+export const signUp = (formData) =>
+  axiosInstance.post('/users/signup', formData);  // 회원가입 요청 처리
+
+export const getUniversities = () =>
+  axiosInstance.get('/universities');
