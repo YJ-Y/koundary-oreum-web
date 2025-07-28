a48d3e89642b2a1309b7b3d23c98ff74f31adbe4
ekfgo0 <anstjsdud0306@gmail.com>
2025-07-28 19:45:09 +0900

    닉네임 아이디 중복확인 오류

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,19 +1,19 @@
 import axios from './axiosInstance';
 
 export const checkNickname = (nickname) =>
-  axios.post('/check-nickname', { nickname });
+  axios.post('/users/check-nickname', { nickname });
 
-export const checkUsername = (ID) =>
-  axios.post('/check-username', { ID });
+export const checkUsername = (loginId) =>
+  axios.post('/users/check-loginId', { loginId });
 
 export const sendVerificationEmail = (email) =>
-  axios.post('/send-verification-email', { email });
+  axios.post('/email/verify-code', { email });
 
 export const verifyCode = (email, code) =>
-  axios.post('/verify-code', { email, code });
+  axios.post('/email/verify-code', { email, code });
 
 export const signUp = (formData) =>
-  axios.post('/signup', formData);
+  axios.post('/users/signup', formData);
 
 export const getUniversities = () =>
-  axios.get('/universities');
+  axios.get('/users/universities');
